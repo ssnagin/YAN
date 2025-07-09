50e33f85e6795857bf45cc8d573a99e26f1f0d4f
ssnagin <56646530+ssnagin@users.noreply.github.com>
2025-07-09 19:36:24 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Yet Another Notepad
+
+A simple text utility, created (will be created) for simple text data manipulation.
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int arg_counter, char *argv[]) {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
